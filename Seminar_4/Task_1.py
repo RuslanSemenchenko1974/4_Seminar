6c74564fd9c4c8f7299669260624068e73880683
Ruslan <89604587433@mail.ru>
2022-12-12 07:03:47 +0300

    домашнее задание 4 семинар исправленное

--- Seminar_4/Task_1.py
+++ Seminar_4/Task_1.py
@@ -2,6 +2,11 @@
 заработной платы сотрудника. В расчете необходимо использовать формулу: 
 (выработка в часах*ставка в час) + премия. Для выполнения расчета для 
 конкретных значений необходимо запускать скрипт с параметрами."""
+from sys import argv
+
+script_name, w_time, h_salary, m_prize = argv
+
+
 def function():
     while True:
         try:
@@ -11,14 +16,21 @@ def function():
         else:
             return x
 
+
 def salary(work_time, hour_salary, prize):
-    return work_time*hour_salary+prize
-print('Введите отработанное сотрудником время : ')
+    return work_time * hour_salary + prize
+
+
+"""print('Введите отработанное сотрудником время : ')
 w_time = function()
 print('Введите з/п сотрудника в час  :')
 h_salary = function()
 print('Введите премию : ')
-m_prize = function()
-print(f'Заработная плат сотрудника : {salary(w_time, h_salary,m_prize)}')
+m_prize = function()"""
+print(script_name)
+w_time = int(w_time)
+h_salary = int(h_salary)
+m_prize = int(m_prize)
+print(f'Заработная плат сотрудника : {salary(w_time, h_salary, m_prize)}')
 
 
